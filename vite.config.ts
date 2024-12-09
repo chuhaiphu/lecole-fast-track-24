9d92f6317d243ca5b6c45ace65f5469815d04b68
doanvinhphu <doanvinhphu321@gmail.com>
2024-12-09 19:55:49 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => ({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer]
