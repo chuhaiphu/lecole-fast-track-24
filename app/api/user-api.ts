8bcf871d3552742a10985e9468b420acd33d5793
doanvinhphu <doanvinhphu321@gmail.com>
2024-12-12 00:17:14 +0700

    test deploy

--- app/api/user-api.ts
+++ app/api/user-api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API_ROOT = 'http://localhost:3000/api'
+const API_ROOT = 'http://0.0.0.0:3000/api'
 
 export const registerUserAPI = async (userData: { username: string; roles: string; secret_phrase: string }) => {
   const response = await axios.post(`${API_ROOT}/users/register`, userData)
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,11 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     proxy: {
-      '/api': 'http://0.0.0.0:3000'
+      '/api': {
+        target: 'http://0.0.0.0:3000',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
   css: {
