17756e4ae55a2e71b1791742bbb4281746b07b34
doanvinhphu <doanvinhphu321@gmail.com>
2024-12-11 22:43:46 +0700

    test deploy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import https from "http";
+import http from "http";
 import { Server } from "socket.io";
 import setupSocketHandlers from './socket/socketHandler.js';
 import userRouter from "./route/userRoute.js";
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app = express();
 const port = 3000;
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 
 app.use(cors());
 const io = new Server(server, {
@@ -21,6 +21,6 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 setupSocketHandlers(io);
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      '/api':  'http://0.0.0.0:3000',
+    }
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer]
